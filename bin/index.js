ee401ba1da9709f6a4b88236ac092fb1ff548f79
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-23 12:09:06 +0700

    conditional flag

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const { program } = require('commander');
 
+// Concat the string to array
 const collect = (value, previous) => previous.concat([value]);
 
 /**
@@ -9,8 +10,9 @@ const collect = (value, previous) => previous.concat([value]);
  */
 program
   .option('-t, --type [type]', 'convert file to json or text', collect, [])
-  .option('-o, --output [output]', 'location save file', collect, []);
+  .option('-o, --output [location]', 'location save file', collect, []);
 
+program.addHelpText('after');
 program.parse();
 
 /**
@@ -18,5 +20,21 @@ program.parse();
  * the key is by long flag and the default value each key is array
  */
 const options = program.opts();
+console.log('options', options);
+// Use flag t and flag o
+if ((options.type && options.type.length > 0) && (options.output && options.output.length > 0)) {
+  console.log('OPTIONS TYPE', options.type);
+  console.log('OPTIONS OUTPUT', options.output);
+}
 
-console.log(options);
+// Use only flag t
+if ((options.type || options.type) && (!options.output || !options.output.length)) {
+  console.log('ONLY FLAG TYPE', options.type);
+  console.log('THE FLAG OUTPUT', options.output);
+}
+
+// Use only flag o
+if ((!options.type || !options.type.length) && (options.output && options.output.length > 0)) {
+  console.log('ONLY FLAG OUTPUT', options.output);
+  console.log('THE FLAG TYPE', options.type);
+}
