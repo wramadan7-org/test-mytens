228f68601b61a6cc27b32ab861d71d59335a0662
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-23 11:31:15 +0700

    use package command and parsing data command program to object

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,22 @@
-#!/usr/bin/env node 
-const yargs = require('yargs');
+#!/usr/bin/env node
+const { program } = require('commander');
 
-const options = yargs
-  .usage("Usage: mytools <command> [options]")
-  .option("l", {
-    alias: "language",
-    description: "Translate to language",
-    type: "string",
-    demandOption: false,
-  })
-  .option("s", {
-    alias: "sentence",
-    description: "Setence to be translate",
-    type: "string",
-    demandOption: false,
-  })
-  .help(true)
-  .argv;
+const collect = (value, previous) => previous.concat([value]);
+
+/**
+ * Set the option cli flag
+ * Only contain that flag -flag for short and --flag for long flag
+ */
+program
+  .option('-t, --type [type]', 'convert file to json or text', collect, [])
+  .option('-o, --output [output]', 'location save file', collect, []);
+
+program.parse();
+
+/**
+ * Set the program option to object with
+ * the key is by long flag and the default value each key is array
+ */
+const options = program.opts();
+
+console.log(options);
