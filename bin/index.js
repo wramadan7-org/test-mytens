bfecde2445dcaefcea1b21f4edb54284ccdcf151
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-24 09:27:09 +0700

    use two flag, use t flag, and use o flag

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
 const { program } = require('commander');
-const { readAndConvertToJson } = require('./logicFlag');
+const {
+  readAndConvertToJson, readAndConvertToText, writeFileToJson, writeFileToText,
+} = require('./logicFlag');
+
+// Default location file log in windows
+const defaultFileLog = 'C:/Windows/Panther/setuperr.log';
+// Default location file output to save
+const defaultFileOutput = 'D:/Project/Test/TestMyTens/files';
+// Input type file on flag t
+const inputTypes = {
+  json: 'json',
+  text: 'text',
+};
 
 // Concat the string to array
 const collect = (value, previous) => previous.concat([value]);
@@ -27,20 +39,113 @@ program.parse();
 const options = program.opts();
 
 // Use flag t and flag o
-if ((options.type && options.type.length > 0) && (options.output && options.output.length > 0)) {
+if ((options.type && options.type.length) && (options.output && options.output.length)) {
   console.log('OPTIONS TYPE', options.type);
   console.log('OPTIONS OUTPUT', options.output);
+  switch (options.type[0]) {
+    // Convert to JSON
+    case inputTypes.json:
+      console.log('Proccess to convert file in JSON...');
+
+      readAndConvertToJson(defaultFileLog);
+
+      console.log('Loading...');
+
+      console.log('Success to convert file LOG to JSON...');
+
+      console.log('Proccess to write file...');
+
+      writeFileToJson(options.output || defaultFileOutput, defaultFileLog);
+
+      console.log('Success to write file to JSON...');
+
+      break;
+
+    case inputTypes.text:
+      console.log('Proccess to convert file in TEXT...');
+
+      readAndConvertToText(defaultFileLog);
+
+      console.log('Loading...');
+
+      console.log('Success to convert file LOG to TEXT...');
+
+      console.log('Proccess to write file...');
+
+      writeFileToText(options.output || defaultFileOutput, defaultFileLog);
+
+      console.log('Success to write file to TEXT...');
+
+      break;
+
+    default:
+      console.log('Proccess to convert file in TEXT...');
+
+      readAndConvertToText(defaultFileLog);
+
+      console.log('Loading...');
+
+      console.log('Success to convert file LOG to TEXT...');
+
+      console.log('Proccess to write file...');
+
+      writeFileToText(options.output || defaultFileOutput, defaultFileLog);
+
+      console.log('Success to write file to TEXT...');
+
+      break;
+  }
 }
 
 // Use only flag t
-if ((options.type || options.type) && (!options.output || !options.output.length)) {
+if ((options.type && options.type.length) && (!options.output || !options.output.length)) {
   console.log('ONLY FLAG TYPE', options.type);
   console.log('THE FLAG OUTPUT', options.output);
-  console.log(readAndConvertToJson());
+  console.log(readAndConvertToJson(defaultFileLog));
+  switch (options.type[0]) {
+    case inputTypes.json:
+      console.log('Proccess to convert file in JSON...');
+
+      console.log('Loading...');
+
+      console.log(readAndConvertToJson(defaultFileLog));
+
+      console.log('Success to convert file LOG to JSON...');
+
+      break;
+
+    case inputTypes.text:
+      console.log('Proccess to convert file in TEXT...');
+
+      console.log('Loading...');
+
+      console.log(readAndConvertToText(defaultFileLog));
+
+      console.log('Success to convert file LOG to TEXT...');
+
+      break;
+
+    default:
+      console.log('Proccess to convert file in TEXT...');
+
+      console.log('Loading...');
+
+      console.log(readAndConvertToText(defaultFileLog));
+
+      console.log('Success to convert file LOG to TEXT...');
+
+      break;
+  }
 }
 
 // Use only flag o
-if ((!options.type || !options.type.length) && (options.output && options.output.length > 0)) {
+if ((!options.type || !options.type.length) && (options.output && options.output.length)) {
   console.log('ONLY FLAG OUTPUT', options.output);
   console.log('THE FLAG TYPE', options.type);
+
+  console.log('Proccess to write file...');
+
+  writeFileToText(options.output || defaultFileOutput, defaultFileLog);
+
+  console.log('Success to write file to TEXT...');
 }
